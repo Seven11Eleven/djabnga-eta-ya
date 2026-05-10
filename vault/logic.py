2e5688bd8aa2bc022b71e5f4a606acf13b028ba6
Seven11Eleven <120821293+Seven11Eleven@users.noreply.github.com>
2026-05-10 12:05:08 +0500

    ispravil, sorry :(

--- vault/logic.py
+++ vault/logic.py
@@ -1,5 +1,5 @@
 def get_part_5():
-    cipher = "wqzmgl_vgl_bz}"
+    cipher = "wqzmtl_vgl_bz}"
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     atbash_map = str.maketrans(alphabet, alphabet[::-1])
     return cipher.translate(atbash_map)
